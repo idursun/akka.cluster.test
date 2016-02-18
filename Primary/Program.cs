fc2ecd507ec71b9fbff2732257e322110232073f
ibrahim dursun <ibrahim.dursun@hepsiburada.com>
2016-02-18 13:09:42 +0200

    initial commit

---
+++ Primary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Routing;
+
+namespace Primary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActorSystem system = ActorSystem.Create("Cluster");
+            var router = system.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "broadcaster");
+
+            while (Console.ReadLine() != "q")
+            {
+                var routees = router.Ask<Routees>(new GetRoutees()).Result;
+                var consistentHashableEnvelope = new ConsistentHashableEnvelope(Guid.NewGuid().ToString(), Guid.NewGuid());
+                router.Tell(consistentHashableEnvelope);
+                Console.WriteLine("sending {0} routees", routees.Members.Count());
+            }
+        }
+    }
+}
---
+++ Worker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka;
+using Akka.Actor;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("Cluster");
+            var worker = system.ActorOf<WorkerActor>("worker");
+            system.WhenTerminated.Wait();
+        }
+    }
+
+    internal class WorkerActor: UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            message.Match()
+                .With<string>(s => Console.WriteLine(s));
+        }
+    }
+}
